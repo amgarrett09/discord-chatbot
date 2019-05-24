1bad95eccaeb798db91f013318bd2faa338f90bf
Alex Garrett <agarrettR8@gmail.com>
2019-05-23 23:39:33 -0400

    implemented basic ping-pong bot

--- src/bot.rs
+++ src/bot.rs
@@ -1,9 +1,50 @@
 use std::sync::mpsc;
 
-pub fn run_bot(tx: &mpsc::Sender<String>, rx: &mpsc::Receiver<String>) {
-    // Wait for application key from main thread
-    let key = match rx.recv() {
+use serenity::client;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+use cursive::views::Dialog;
+use cursive::Cursive;
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn message(&self, _: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say("Pong!") {
+                eprintln!("Error sending message: {:?}", why);
+            }
+        }
+    }
+}
+
+fn send_error_message(msg: &'static str, cb_sink: &cursive::CbSink) {
+    let err_msg = String::from(msg);
+    cb_sink.send(Box::new(|a: &mut Cursive| {
+        a.add_layer(Dialog::text(err_msg)
+            .button("Quit", Cursive::quit));
+    })).unwrap();
+}
+
+pub fn run_bot(_tx: &mpsc::Sender<String>, rx: &mpsc::Receiver<String>, cb_sink: cursive::CbSink) {
+    // Wait for application token from main thread
+    let token = match rx.recv() {
         Ok(st) => st,
-        Err(_) => return
+        Err(_) => {
+            send_error_message("Error: couldn't read application token.", &cb_sink);
+            return;
+        }
     };
+
+    if client::validate_token(&token).is_err() {
+        send_error_message("Error: invalid token", &cb_sink);
+        return;
+    }
+
+    let mut client = Client::new(&token, Handler).expect("Failed to create client");
+
+    if client.start().is_err() {
+        send_error_message("Error: failed to start client.", &cb_sink);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 mod bot;
 
-use cursive::Cursive;
-use cursive::views::{Dialog, LinearLayout, DummyView, TextView, EditView};
 use cursive::traits::*;
+use cursive::views::{Dialog, DummyView, EditView, LinearLayout, TextView};
+use cursive::Cursive;
 
-use std::path::Path;
 use std::fs::File;
 use std::io::prelude::*;
-use std::thread;
+use std::path::Path;
 use std::sync::mpsc;
+use std::thread;
 
 fn main() {
     let mut app = Cursive::default();
 
-    app.set_user_data(Data { key: String::new() });
+    app.set_user_data(Data {
+        token: String::new(),
+    });
 
     // Sender goes to backend thread. Messages travel backend -> frontend.
     let (txb, _): (mpsc::Sender<String>, mpsc::Receiver<String>) = mpsc::channel();
@@ -21,111 +23,112 @@ fn main() {
     // Receiver goes to backend thread. Messages travel frontend -> backend.
     let (txf, rxb): (mpsc::Sender<String>, mpsc::Receiver<String>) = mpsc::channel();
 
+    let cb_sink = app.cb_sink().clone();
+
     thread::spawn(move || {
-        bot::run_bot(&txb, &rxb);
+        bot::run_bot(&txb, &rxb, cb_sink);
     });
 
     main_menu(&mut app, txf);
-    check_for_key(&mut app);
+    check_for_token(&mut app);
 
     app.run();
 }
 
 struct Data {
-    key: String
+    token: String,
 }
 
-fn check_for_key(app: &mut Cursive) {
-    let path = Path::new("key.cfg");
+fn check_for_token(app: &mut Cursive) {
+    let path = Path::new("tokens.cfg");
 
-    // Buffer for key config file contents
+    // Buffer for token config file contents
     let mut buffer = String::new();
 
-    // Try to open key config file and read from it, if it exists
+    // Try to open token config file and read from it, if it exists
     let file = File::open(&path);
     if let Ok(f) = file {
         let mut file = f;
 
         if let Err(why) = file.read_to_string(&mut buffer) {
-            panic!("Couldn't read from key config file: {:?}", why);
+            panic!("Couldn't read from token config file: {:?}", why);
         }
     }
 
-    // If we didn't find a key
+    // If we didn't find a token
     if !buffer.contains(":") {
-        let edit_name = EditView::new()
-            .with_id("app_name");
+        let edit_name = EditView::new().with_id("app_name");
 
-        let edit_key = EditView::new()
-            .with_id("key");
+        let edit_token = EditView::new().with_id("token");
 
-        let text1 = TextView::new("We couldn't find an existing application key, \
-            so you'll need to enter a name for your bot and its application key now.\
-            \n\n\
-            Enter your bot's name:")
-            .fixed_width(25);
+        let text1 = TextView::new(
+            "We couldn't find an existing application token, \
+             so you'll need to enter a name for your bot and its application token.\
+             \n\n\
+             Enter your bot's name:",
+        )
+        .fixed_width(25);
 
         let layout = LinearLayout::vertical()
             .child(text1)
             .child(edit_name)
             .child(DummyView)
-            .child(TextView::new("Enter the application key:"))
-            .child(edit_key);
+            .child(TextView::new("Enter the application token:"))
+            .child(edit_token);
 
         let dialog = Dialog::around(layout)
             .title("Enter bot's name")
             .button("Ok", |a| {
-                let name = a.call_on_id("app_name", |view: &mut EditView| {
-                    view.get_content()
-                }).unwrap();
+                let name = a
+                    .call_on_id("app_name", |view: &mut EditView| view.get_content())
+                    .unwrap();
 
-                let key = a.call_on_id("key", |view: &mut EditView| {
-                    view.get_content()
-                }).unwrap();
+                let token = a
+                    .call_on_id("token", |view: &mut EditView| view.get_content())
+                    .unwrap();
 
-                ok(a, &name, &key);
+                ok(a, &name, &token);
             })
             .button("Quit", Cursive::quit);
 
         app.add_layer(dialog);
     } else {
-        // If we do find a key, put it in user storage
-        let fields: Vec<&str>  = buffer.split(":").collect();
+        // If we do find a token, put it in user storage
+        let fields: Vec<&str> = buffer.split(":").collect();
 
-        let key_slice = fields[fields.len() - 1];
+        let token_slice = fields[fields.len() - 1];
 
-        let key = format!{"{}", key_slice.trim()};
+        let token = format! {"{}", token_slice.trim()};
 
         app.with_user_data(|data: &mut Data| {
-            data.key = key;
+            data.token = token;
         });
     }
 
     // Private function which runs when we hit "Okay" on the dialog above
-    fn ok(app: &mut Cursive, name: &str, key: &str) {
-        let s: String = format!("{}: {}\n", name, key);
+    fn ok(app: &mut Cursive, name: &str, token: &str) {
+        let s: String = format!("{}: {}\n", name, token);
 
-        let path = Path::new("key.cfg");
+        let path = Path::new("tokens.cfg");
         let mut file = match File::create(&path) {
             Ok(file) => file,
-            Err(why) => panic!("Couldn't open key config file: {:?}", why)
+            Err(why) => panic!("Couldn't open token config file: {:?}", why),
         };
 
         if let Err(why) = file.write_all(s.as_bytes()) {
-            panic!("Couldn't write key to file: {:?}", why);
+            panic!("Couldn't write token to file: {:?}", why);
         }
 
-        // Save key in user data
+        // Save token in user data
         app.with_user_data(|data: &mut Data| {
-            data.key = format!{"{}", key};
+            data.token = format! {"{}", token};
         });
 
         app.pop_layer();
 
-        app.add_layer(Dialog::text("Saved application key!")
-            .button("Ok", |a| {
-                a.pop_layer();
-            }));
+        app.add_layer(Dialog::text("Saved application token!").button("Ok", |a| {
+            a.pop_layer();
+        }));
     }
 }
 
@@ -133,8 +136,8 @@ fn main_menu(app: &mut Cursive, tx: mpsc::Sender<String>) {
     let dialog = Dialog::text("When you're ready to launch, hit the button...")
         .title("Welcome")
         .button("Launch bot", move |a| {
-            let key = &a.user_data::<Data>().unwrap().key;
-            tx.send(key.to_string()).unwrap();
+            let token = &a.user_data::<Data>().unwrap().token;
+            tx.send(token.to_string()).unwrap();
         })
         .button("Quit", Cursive::quit);
 
