ecba3ff58e474db196af896b2e218f27ca96e0c5
Alex Garrett <agarrettR8@gmail.com>
2019-05-27 11:59:58 -0400

    updated gitignore

--- chatbot_macros/target/debug/deps/chatbot_macros-ae1357dbf231de13.d
+++
@@ -1,5 +0,0 @@
-/home/alex/Documents/programming_projects/rust-chatbot/chatbot_macros/target/debug/deps/chatbot_macros-ae1357dbf231de13.rmeta: src/lib.rs
-
-/home/alex/Documents/programming_projects/rust-chatbot/chatbot_macros/target/debug/deps/chatbot_macros-ae1357dbf231de13.d: src/lib.rs
-
-src/lib.rs:
